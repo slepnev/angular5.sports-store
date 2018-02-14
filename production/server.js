cd14099bff0e812cd6da10eb7e736ba44e661421
alexander <a.slepnev@it365.su>
2018-02-14 17:24:01 +0300

    Prodaction

--- production/server.js
+++ production/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.use("/node_modules", express.static("/usr/src/sportsstore/node_modules"));
-app.use("/", express.static("/usr/src/sportsstore/app"));
+app.use("/node_modules", express.static("./node_modules"));
+app.use("/", express.static("./app"));
 
 app.listen(3000, function () {
   console.log("HTTP Server running on port 3000");
